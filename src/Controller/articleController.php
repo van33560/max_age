bc8b9b2ccd054902d25591256d05608956fc38f9
vanessa <vanlab33@hotmail.fr>
2020-11-17 12:28:45 +0100

    wildcard

--- src/Controller/articleController.php
+++ src/Controller/articleController.php
@@ -2,22 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class articleController
 { /**
- * @Route("/article", name = "page_article")
+ * @Route("/article/{id}", name = "page_article")//pour récupérer les id apres article /{id}"whild card
  */
-    public function articleShowAction(Request $request)
+    public function articleShowAction($id)//cette variable contiendra le numéro  de l'id
     {
-
-        $idArticle = ($request->query->get('id'));
-
         //tableau
-         $articles = [
+         $article = [
              1 => 'Article 1',
              2 => "Article 2",
              3 => "Article 3",
@@ -25,8 +22,8 @@ class articleController
              5 => "Article 5",
              6 => "Article 6",
          ];
-       //reponse qui contient id+articles
-       $response = new response('<h1> ' . $articles[$idArticle] . '</h1>');
+       //reponse qui contient l'id de l'article
+       $response = new response('<h1> ' . $article[$id] . '</h1>');
        //retour reponse
        return $response;
    }
