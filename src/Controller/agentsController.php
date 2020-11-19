31ed70784b1f06d516d7653de12a8f5ff7156397
vanessa <vanlab33@hotmail.fr>
2020-11-19 11:54:08 +0100

    id

--- src/Controller/agentsController.php
+++ src/Controller/agentsController.php
@@ -9,7 +9,62 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class agentsController extends AbstractController
+//je crée une nouvelle route pour recuperer id
 {   /**
+    * @Route("/agent/{id}", name="agentShow")
+    */
+// je crée une nouvelle méthode qui va permettre de récupérer le contenu de chaque id
+    public function agentShow($id){
+
+        $agents = [
+            1 => [
+                "id" =>1,
+                "lastName" => "Robert",
+                "firstName" => "David",
+                "age" => 30,
+                "published" => true
+            ],
+            2 => [
+                "id" =>2,
+                "lastName" => "Labaste",
+                "firstName" => "Denis",
+                "age" => 29,
+                "published" => true
+            ],
+            3 => [
+                "id" =>3,
+                "lastName" => "Rozand",
+                "firstName" => "Mathieu",
+                "age" => 31,
+                "published" => false
+            ],
+            4 => [
+                "id" =>4,
+                "lastName" => "Despert",
+                "firstName" => "Yoann",
+                "age" => 33,
+                "published" => true
+            ],
+            5 => [
+                "id" =>5,
+                "lastName" => "Dorignac",
+                "firstName" => "Loic",
+                "age" => 34,
+                "published" => false
+            ]
+        ];
+        $agent = $agents[$id];
+        //je renvoi sur ma page twig les infos de mon tableau par id
+        return $this->render('agent.html.twig',[
+            'agent'=> $agent
+
+        ]);
+
+    }
+
+
+
+    /**
      * @Route("/agents", name="agents")
      */
 
@@ -18,30 +73,36 @@ class agentsController extends AbstractController
     {
         $agents = [
             1 => [
+                "id" =>1,
                 "lastName" => "Robert",
                 "firstName" => "David",
                 "age" => 30,
                 "published" => true
             ],
             2 => [
+
+                "id" =>2,
                 "lastName" => "Labaste",
                 "firstName" => "Denis",
                 "age" => 29,
                 "published" => true
             ],
             3 => [
+                "id" =>3,
                 "lastName" => "Rozand",
                 "firstName" => "Mathieu",
                 "age" => 31,
                 "published" => false
             ],
             4 => [
+                "id" =>4,
                 "lastName" => "Despert",
                 "firstName" => "Yoann",
                 "age" => 33,
                 "published" => true
             ],
             5 => [
+                "id" =>5,
                 "lastName" => "Dorignac",
                 "firstName" => "Loic",
                 "age" => 34,
